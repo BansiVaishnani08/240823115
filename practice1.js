b2a40d515b79c1aaa1c07cfb31f5795d85e759d2
BansiVaishnani08 <bansivaishnani08@gmail.com>
2025-07-14 10:58:53 +0530

    Practice set 1 started

---
+++ practice1.js
@@ -0,0 +1,14 @@
+// console.log("Hello World")
+
+// const name="Bansi Vaishnani";
+// console.log(name);
+
+const num=7
+if(num%2==0)
+{
+    console.log(num + " Number is EVEN")
+}
+else
+{
+    console.log(num + "Number is ODD")
+}
